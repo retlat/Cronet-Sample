c79827e106aa43c1c584f261c6bb129ada77dd29
retlat <16059710+retlat@users.noreply.github.com>
2020-09-06 17:38:11 +0900

    Add injection provide config.

--- app/src/main/java/com/example/sample/di/AppModule.kt
+++ app/src/main/java/com/example/sample/di/AppModule.kt
@@ -1,6 +1,10 @@
 package com.example.sample.di
 
 import android.content.Context
+import com.example.sample.data.DefaultJSONCallback
+import com.example.sample.data.DefaultSampleDataSource
+import com.example.sample.data.JSONCallback
+import com.example.sample.data.SampleDataSource
 import com.example.sample.util.CronetInstaller
 import com.google.android.gms.common.GoogleApiAvailability
 import dagger.Module
@@ -8,6 +12,8 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import org.chromium.net.CronetEngine
 
 @Module
@@ -31,5 +37,25 @@ object AppModule {
             .enableQuic(true)
             .enableBrotli(true)
             .build()
+    }
+
+    @IODispatcher
+    @Provides
+    fun provideIODispatcher(): CoroutineDispatcher {
+        return Dispatchers.IO
+    }
+
+    @Provides
+    fun provideJSONCallback(): JSONCallback {
+        return DefaultJSONCallback()
+    }
+
+    @Provides
+    fun provideSampleDataSource(
+        engine: CronetEngine,
+        callback: JSONCallback,
+        @IODispatcher dispatcher: CoroutineDispatcher
+    ): SampleDataSource {
+        return DefaultSampleDataSource(engine, callback, dispatcher)
     }
 }
