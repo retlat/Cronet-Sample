0752969ee50a1a774fb20f0f6ff65840e12206ce
retlat <16059710+retlat@users.noreply.github.com>
2020-09-06 14:27:02 +0900

    Add layout for InitialFragment.

--- app/src/main/java/com/example/sample/InitialFragment.kt
+++ app/src/main/java/com/example/sample/InitialFragment.kt
@@ -1,5 +1,30 @@
 package com.example.sample
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.sample.databinding.FragmentInitialBinding
 
-class InitialFragment : Fragment()
+class InitialFragment : Fragment() {
+
+    private var _binding: FragmentInitialBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentInitialBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        _binding = null
+    }
+}
