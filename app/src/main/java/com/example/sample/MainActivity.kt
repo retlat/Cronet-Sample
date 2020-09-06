d2b07ff3a8c96ae5c667cf45bc628f37faf2f599
retlat <16059710+retlat@users.noreply.github.com>
2020-09-06 14:19:04 +0900

    Implement Play Service and Cronet installation.

--- app/src/main/java/com/example/sample/MainActivity.kt
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -2,12 +2,37 @@ package com.example.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sample.util.CronetInstaller
+import com.google.android.gms.common.GoogleApiAvailability
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var googleApiAvailability: GoogleApiAvailability
+
+    @Inject
+    lateinit var installer: CronetInstaller
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Check availability here because this app requires Play Service and Cronet
+        // but user can uninstall / force stop while it life cycle is in created state.
+        googleApiAvailability.makeGooglePlayServicesAvailable(this)
+            .addOnFailureListener { finish() }
+            .addOnSuccessListener {
+                installer.install(this)
+                    .addOnSuccessListener { TODO() }
+                    .addOnFailureListener { finish() }
+            }
     }
 }
