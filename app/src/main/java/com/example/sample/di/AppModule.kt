be55a2bac3555e6c14db33d7510e5ce8e4c87c79
retlat <16059710+retlat@users.noreply.github.com>
2020-09-06 15:24:44 +0900

    Add CronetEngine provide config.

--- app/src/main/java/com/example/sample/di/AppModule.kt
+++ app/src/main/java/com/example/sample/di/AppModule.kt
@@ -1,11 +1,14 @@
 package com.example.sample.di
 
+import android.content.Context
 import com.example.sample.util.CronetInstaller
 import com.google.android.gms.common.GoogleApiAvailability
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import org.chromium.net.CronetEngine
 
 @Module
 @InstallIn(ApplicationComponent::class)
@@ -19,5 +22,14 @@ object AppModule {
     @Provides
     fun provideCronetInstaller(): CronetInstaller {
         return CronetInstaller()
+    }
+
+    @Provides
+    fun provideCronetEngine(@ApplicationContext context: Context): CronetEngine {
+        return CronetEngine.Builder(context)
+            .enableHttp2(true)
+            .enableQuic(true)
+            .enableBrotli(true)
+            .build()
     }
 }
