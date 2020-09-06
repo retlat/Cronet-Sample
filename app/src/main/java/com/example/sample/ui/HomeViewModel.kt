9a578e8ed2e748b0bd8f4ae6606e46e968d23240
retlat <16059710+retlat@users.noreply.github.com>
2020-09-06 17:39:16 +0900

    Implement post method.

--- app/src/main/java/com/example/sample/ui/HomeViewModel.kt
+++ app/src/main/java/com/example/sample/ui/HomeViewModel.kt
@@ -1,10 +1,21 @@
 package com.example.sample.ui
 
+import android.content.Context
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sample.R
+import com.example.sample.data.SampleDataSource
+import com.example.sample.util.RequestResult
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel @ViewModelInject constructor(
+    private val source: SampleDataSource,
+    @ApplicationContext private val context: Context
+) : ViewModel() {
 
     private val _url = MutableLiveData("https://your.endpoint.example.com/")
     val url: LiveData<String> get() = _url
@@ -12,7 +23,29 @@ class HomeViewModel : ViewModel() {
     private val _response = MutableLiveData("")
     val response: LiveData<String> get() = _response
 
+    private val _loading = MutableLiveData(false)
+    val loading: LiveData<Boolean> get() = _loading
+
     fun onClickPostButton() {
-        TODO()
+        viewModelScope.launch {
+            _loading.value = true
+            _response.value = ""
+
+            when (val result = source.post(url.value!!)) {
+                is RequestResult.Success -> _response.value = result.data
+                else -> {
+                    val id = when (result) {
+                        is RequestResult.NotFound -> R.string.home_result_404
+                        is RequestResult.InternalServerError -> R.string.home_result_500
+                        is RequestResult.ServiceUnavailable -> R.string.home_result_503
+                        else -> R.string.home_result_else
+                    }
+
+                    _response.value = context.getString(id)
+                }
+            }
+
+            _loading.value = false
+        }
     }
 }
